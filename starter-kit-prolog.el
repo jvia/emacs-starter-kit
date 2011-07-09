1948306d8815988f31533dc41470225e1452944f
Jeremiah Via <jeremiah.via@gmail.com>
2011-07-09 14:19:16 +0100

    Add support for Prolog.

--- starter-kit-prolog.el
+++ starter-kit-prolog.el
@@ -3,7 +3,20 @@
 ;;
 ;; Not part of the starter kit.
 
-(load "/usr/local/sicstus4.2.0/lib/sicstus-4.2.0/emacs/sicstus_emacs_init.el")
-(add-to-list 'auto-mode-alist '("\\.pl$" . prolog-mode))
+;; Load prolog.el
+(add-to-list 'load-path (concat dotfiles-dir "src"))
+
+;; Setup modes
+(autoload 'run-prolog   "prolog" "Start a Prolog sub-process." t)
+(autoload 'prolog-mode  "prolog" "Major mode for editing Prolog programs." t)
+(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
+
+;; Use SWI-Prolog
+(setq prolog-system 'swi)
+(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
+                                ("\\.m$" . mercury-mode))
+                              auto-mode-alist))
+
+
 (provide 'starter-kit-prolog)
 
