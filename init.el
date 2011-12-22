13b7ba6540a0acf56b89a2ba5705d84f2b9f1020
Jeremiah Via <jeremiah.via@gmail.com>
2011-12-22 15:17:42 -0800

    Added org-mode and gnus stuff.

--- init.el
+++ init.el
@@ -20,96 +20,18 @@
 ;; load up the starter kit
 (org-babel-load-file (expand-file-name "emacs.org" starter-kit-dir))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doc-view-continuous t)
+ '(org-agenda-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;;; init.el ends here
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;;                            personal stuff                       ;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; Color theme
-;; (color-theme-sanityinc-solarized-dark)
-;; (global-set-key (kbd "M-q") 'fill-paragraph)
-;; (setq column-number-mode t)
-;; (global-set-key (kbd "S-p") 'hyperspec-lookup)
-;; (global-set-key [f1] 'ibuffer)
-
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;;                            yasnippet                            ;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (setq yas/prompt-functions '(yas/ido-prompt yas/dropdown-prompt))
-
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;;                        Settings for C/C++                       ;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (setq c-default-style '((java-mode . "java")
-;;                         (awk-mode  . "awk")
-;;                         (other     . "linux")))
-;; ;; Automatically reindent current line after 'punctuation' character.
-;; (setq-default c-electric-flag t)
-;; (setq c-basic-offset 4)
-;; ;; Documentation
-;; (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;;                      Settings for Lisp                          ;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (add-to-list 'load-path "~/.emacs.d/jxv911/slime/")
-;; (require 'slime)
-;; (if (eq system-type 'darwin)
-;;     (progn
-;;       (setq inferior-lisp-program "sbcl")
-;;       (slime-setup '(slime-fancy))))
-
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;;                           Projects                             ;;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (setq org-publish-project-alist
-;;       '(("org-via"
-;;          ;; Path to your org files.
-;;          :base-directory "~/Sites/jeremiahvia.com/_org/"
-;;          :base-extension "org"
-;;          :exclude "md\\|markdown"
-
-;;          ;; Path to your Jekyll project.
-;;          :publishing-directory "~/Sites/jeremiahvia.com/jekyll/"
-;;          :recursive t
-;;          :publishing-function org-publish-org-to-html
-;;          :headline-levels 4
-;;          :html-extension "html"
-;;          :body-only t ;; Only export section between <body> </body>
-;;          )
-
-;;         ("org-static-via"
-;;          :base-directory "~/Sites/jeremiahvia.com/_org/"
-;;          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php\\|ico"
-;;          :publishing-directory "~/Sites/jeremiahvia.com/jekyll/"
-;;          :recursive t
-;;          :publishing-function org-publish-attachment
-;;          )
-
-;;         ;; Publish everything
-;;         ("via" :components ("org-via" "org-static-via")))
-;;       )
-
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;                   Emacs generated custom stuff                   ;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(doc-view-continuous t)
-;;  '(org-agenda-files nil))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
-
-
-;; ;;; init.el ends here
