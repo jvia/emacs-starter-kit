91243537a83691ed299dbf5f51210752564464a3
Jeremiah Via <jeremiah.via@gmail.com>
2012-04-20 17:18:36 -0500

    Commented out most of the config file. Going to create a new one from the ground up.

--- init.el
+++ init.el
@@ -7,9 +7,8 @@
 
 ;; Turn off mouse interface early in startup to avoid momentary display
 ;; You really don't need these; trust me.
-;;; (unless (eq system-type 'darwin))
-
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(unless (eq system-type 'darwin)
+  (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
@@ -19,19 +18,5 @@
 
 ;; load up the starter kit
 (org-babel-load-file (expand-file-name "emacs.org" starter-kit-dir))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doc-view-continuous t)
- '(org-agenda-files nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;;; init.el ends here
